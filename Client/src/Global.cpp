0fa1ab0db1004d8bc079a55005df9e2e4365e2e9
snap_user <snap_user@8f322a54-0e1d-4745-a648-d3e5bdbf05c0>
2008-06-03 03:59:19 +0000

    - set version to 1.0.4

--- Client/src/Global.cpp
+++ Client/src/Global.cpp
@@ -14,7 +14,7 @@ Poco::Mutex     Global::_localLogMutex;
 Global*         Global::_instance = 0;
 
 
-Global::            Global(void) : isDebugOn(false), version("1.0.3")
+Global::            Global(void) : isDebugOn(false), version("1.0.4")
 //Global is a singleton.  Keep this a private contructor and use getInstance().
 { 
 }
--- Server/Global.cs
+++ Server/Global.cs
@@ -196,7 +196,7 @@ namespace FRESCO_Server
         public event SimulationFailedEventHandler SimulationFailedEvent;
         public event ClientOutputEventHandler ClientOutputEvent;
         public readonly string Title = "FRESCO Server";
-        public readonly string Version = "1.0.3";
+        public readonly string Version = "1.0.4";
         public readonly int UdpPort = 9050;
         public readonly int TcpPort = 9051;
         public readonly short asNull = -1;
