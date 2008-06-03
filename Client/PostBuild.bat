4a57ce640d443015c605e4dc643792d195e19430
snap_user <snap_user@8f322a54-0e1d-4745-a648-d3e5bdbf05c0>
2008-06-03 03:11:58 +0000

    - removed eclipse project files - renamed "source" folders to "src" for poco make

--- Client/PostBuild.bat
+++ Client/PostBuild.bat
@@ -3,7 +3,7 @@ echo copy mspdb80.dll to editbin dir
 copy "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\mspdb80.dll" "C:\Program Files\Microsoft Visual Studio 8\VC\bin"
 
 echo Add 3GB Flag to EXE Header
-start "Adding 3GB Flag to EXE Header" /D"C:\Program Files\Microsoft Visual Studio 8\VC\bin\" /WAIT editbin.exe "P:\Fresco\Client\Release\Client.exe" /LARGEADDRESSAWARE
+start "Adding 3GB Flag to EXE Header" /D"C:\Program Files\Microsoft Visual Studio 8\VC\bin\" /WAIT editbin.exe "P:\Fresco\trunk\Client\Release\Client.exe" /LARGEADDRESSAWARE
 
 echo delete mspdb80.dll from editbin dir
 del  "C:\Program Files\Microsoft Visual Studio 8\VC\bin\mspdb80.dll"
