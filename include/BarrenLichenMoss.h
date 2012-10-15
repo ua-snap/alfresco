398c19cb663fe27afb2f51a2b658347735fbf9a9
Alec Bennett <alec@alecbennett.com>
2012-10-15 13:51:38 -0800

    Initial addition of BarrenLichenMoss class, integration to follow

---
+++ include/BarrenLichenMoss.h
@@ -0,0 +1,46 @@
+#ifndef Fresco_BarrenLichenMoss_h
+#define Fresco_BarrenLichenMoss_h
+
+
+#include "Frame.h"
+
+
+class FrescoFoundation_API BarrenLichenMoss : public Frame
+/*
+ * Intended primarily as a placeholder currently.  Barren-Lichen-Moss has an 
+ * extremely low likelihood of burn, so at this time, the class is intended
+ * as a way to track this veg type, and little else.
+ */
+{
+public:
+    BarrenLichenMoss(const int& rAge=0,
+          const bool& rIsTopoComplex=false,
+          const float& rSite=0.,
+          const int& rYearOfLastBurn=-1,
+          const int& rLastBurnSeverity=0,
+          const float& rFireIgnitionFactor=0.,
+          const float& rFireSensitivity=0.,
+          const Species& rSpeciesSubCanopy=gBarrenLichenMossID);
+    BarrenLichenMoss(const Frame& rFrame);
+    ~BarrenLichenMoss() {}
+    static void				clear();
+    Frame*					success (Landscape* pParent) {
+        return NULL;
+    }
+    const unsigned char		type () const {
+        return gBarrenLichenMossID;
+    }
+    const float				getHumanIgnitionProb() const {
+        return _humanIgnitionsProb;
+    }
+private:
+    float					getFireProb (const Landscape* pLandscape) {
+        return 0.;
+    }
+    void					historicalFire (Landscape* pParent) {
+        return;
+    }
+};
+
+
+#endif
---
+++ src/BarrenLichenMoss.cpp
@@ -0,0 +1,32 @@
+// BarrenLichenMoss.cpp
+
+
+#include "PreCompiled.h"
+#include "BarrenLichenMoss.h"
+
+
+BarrenLichenMoss::BarrenLichenMoss (const Frame& rFrame)
+    : Frame(rFrame)
+{
+    _yearEstablished         = gYear;
+    _yearFrameEstablished	= gYear;
+}
+
+
+BarrenLichenMoss::BarrenLichenMoss (const int& rAge,
+              const bool& rIsTopoComplex,
+              const float& rSite,
+              const int& rYearOfLastBurn,
+              const int& rLastBurnSeverity,
+              const float& rFireIgnitionFactor,
+              const float& rFireSensitivity,
+              const Species& rSpeciesSubCanopy)
+    : Frame (rAge, rIsTopoComplex, rSite, rYearOfLastBurn, rLastBurnSeverity, rFireIgnitionFactor, rFireSensitivity, rSpeciesSubCanopy)
+{
+}
+
+
+void BarrenLichenMoss::clear()
+//Clear existing run if any and return to before a run is specified.
+{
+}
