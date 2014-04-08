2a1509a2ae2b2bee80b4b933b15f9f3cc879e598
Alec Bennett <alec@alecbennett.com>
2014-04-08 11:13:45 -0800

    Basic commenting

--- include/FIF.h
+++ include/FIF.h
@@ -1,13 +1,17 @@
 #ifndef FIF_H
 #define FIF_H
+/**
+ * This file is used to parse the input configuration file, and create a commonly
+ * accessible config object for use throughout the simulation. File input is 
+ * expected to be in the JSON format. FIF is a holdover from the old format
+ * refrenced as Fire Input File.
+ * */
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
 #include "jsoncpp/json/json.h"
-
-
 
 class FIF {
 	public:
--- src/FIF.cpp
+++ src/FIF.cpp
@@ -5,21 +5,24 @@ FIF::FIF(){
 
 }
 void FIF::Initialize(std::string path, std::string fifName){
+	/* Initialize the object, set input file name, parse JSON file */
 	std::ifstream fif(fifName.c_str(), std::ifstream::binary);
 	bool parsed = reader.parse(fif, root, false);
 	
 	if(!parsed) {
-		std::cout << "Error parsing JSON" << "\n" << reader.getFormatedErrorMessages() << "\n";
+		std::cout << "Error parsing configuration file: " << fifName << "\n" << reader.getFormatedErrorMessages() << "\n";
 	}
-//	std::cout << root.toStyledString() << "\n";
 }
 std::string FIF::fileName(){
+	/* Return the file name */
 	return _fileName;
 }
 bool FIF::CheckKey(Json::Value& n){
+	/* Check if configuration key exists */	
 	return !n.isNull();
 }
 int FIF::pnGet(Json::Value& n, int*& ptr){
+	/* Reference the array for specified 'integer' configuration item to ptr, return length of array */
 	ptr = new int[n.size()];
 	for (int i = 0; i < n.size(); i++){
 		ptr[i] = n[i].asInt();
@@ -27,6 +30,7 @@ int FIF::pnGet(Json::Value& n, int*& ptr){
 	return n.size();
 }
 int FIF::psGet(Json::Value& n, std::string *&ptr){
+	/* Reference the array for specified 'string' configuration item to ptr, return length of array */
 	ptr = new std::string[n.size()];
 	for (int i = 0; i < n.size(); i++){
 		ptr[i] = n[i].asString();
@@ -34,6 +38,7 @@ int FIF::psGet(Json::Value& n, std::string *&ptr){
 	return n.size();
 }
 int FIF::pdGet(Json::Value& n, double*& ptr){
+	/* Reference the array for specified 'double' configuration item to ptr, return length of array */
 	ptr = new double[n.size()];
 	for (int i = 0; i < n.size(); i++){
 		ptr[i] = n[i].asDouble();
@@ -41,6 +46,7 @@ int FIF::pdGet(Json::Value& n, double*& ptr){
 	return n.size();
 }
 int FIF::pbGet(Json::Value& n, bool*& ptr){
+	/* Reference the array for specified 'boolean' configuration item to ptr, return length of array */
 	ptr = new bool[n.size()];
 	for (int i = 0; i < n.size(); i++){
 		ptr[i] = n[i].asBool();
